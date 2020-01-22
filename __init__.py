ef15d1bbaf6fa741e466a236aa0dc6976eee0d8b
Usama Subhani <reachusama@protonmail.com>
2020-01-22 22:39:09 +0500

    Setup Database

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(150), nullable=True)
+    status = db.Column(db.Boolean)
