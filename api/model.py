ed2087b8e2b07fb6ffaa055169f0d091b482b4ea
Usama Subhani <reachusama@protonmail.com>
2020-01-27 21:23:05 +0500

    Non-unique title
    Title may non be unique obviously

--- api/model.py
+++ api/model.py
@@ -3,6 +3,6 @@ from api import db
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(80), unique=False, nullable=False)
     description = db.Column(db.String(150), nullable=True)
     status = db.Column(db.Boolean)
