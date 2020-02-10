04f12f6f0d9df1ed11161317c450bbc218bcaa7e
Usama Subhani <reachusama@protonmail.com>
2020-02-10 19:25:01 +0500

    Fixed returned json

--- api/routes.py
+++ api/routes.py
@@ -12,18 +12,15 @@ def config_routes(app, db):
     def root():  
         return "Hello Worldddd"
 
-
-    # Retrieve Tasks
-    @app.route(base_url + "tasks")
+    @app.route("/todo/api/v1.0/tasks")
     def tasks_list():
         t = Task.query.all()
-        tasks_dict = {"tasks":[]}
+        tasks = []
         for task in t:
             t_dict = task.__dict__ ## TO-DO : Use vars
             t_dict.pop('_sa_instance_state')
-            tasks_dict["tasks"].append(t_dict)
-        return json.dumps(tasks_dict)
-
+            tasks.append(t_dict)
+        return json.dumps(tasks)
 
     @app.route(base_url + "tasks/<task_id>")
     def get_task(task_id):
